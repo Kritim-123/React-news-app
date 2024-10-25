a713668006c780378e696737b67d79a40420cb24
raged_rhombus <kritimbastola2005@gmail.com>
2024-10-24 19:55:02 -0600

    Implemented react router dom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,92 @@
 import './App.css';
-
-
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+
+import {
+  Route,
+  Routes,
+  BrowserRouter,
+} from "react-router-dom";
+
 
 export default class App extends Component {
   
   render() {
     return (
+
       <div>
-        <NavBar />
-        <News pageSize={5} category="science"/>
-      </div>
+      <BrowserRouter>
+        <div>
+          <NavBar />
+          <Routes>
+            <Route 
+              path = "/"
+              element={
+                <News key="general" pageSize={5} category="general"/> 
+              }
+            />
+
+            <Route 
+              path = "/business"
+              element={
+                <News key="business" pageSize={5} category="business"/> 
+              }
+            />
+
+            <Route 
+              path = "/entertainment"
+              element={
+                <News key="entertainment" pageSize={5} category="entertainment"/> 
+              }
+            />
+
+            <Route 
+              path = "/general"
+              element={
+                <News key="general" pageSize={5} category="general"/> 
+              }
+            />
+
+
+            <Route 
+              path = "/health"
+              element={
+                <News key="health" pageSize={5} category="health"/> 
+              }
+            />
+
+
+            <Route 
+              path = "/science"
+              element={
+                <News key="science" pageSize={5} category="science"/> 
+              }
+            />
+
+
+            <Route 
+              path = "/sports"
+              element={
+                <News key="sports" pageSize={5} category="sports"/> 
+              }
+            />
+
+
+            <Route 
+              path = "/technology"
+              element={
+                <News key="technology" pageSize={5} category="technology"/> 
+              }
+            />
+
+
+          </Routes>
+        </div>
+      </BrowserRouter>
+
+
+    </div>
     )
   }
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import {Link} from "react-router-dom"
 
 export class NavBar extends Component {
   static propTypes = {
@@ -11,51 +11,51 @@ export class NavBar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg bg-dark">
             <div className="container-fluid">
-            <a className="navbar-brand text-white" href="/">News Monkey</a>
+            <Link className="navbar-brand text-white" to="/">News Monkey</Link>
+
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
+
             <div className="collapse navbar-collapse " id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              
                 <li className="nav-item">
-                <a className="nav-link active text-white" aria-current="page" href="/">Home</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link text-white" href="/">About</a>
+                <Link className="nav-link active text-white" aria-current="page" to="/">Home</Link>
                 </li>
 
                 <li className="nav-item">
-                <a className="nav-link text-white" href="/">Business</a>
+                <Link className="nav-link text-white" to="/business">Business</Link>
                 </li>
 
                 <li className="nav-item">
-                <a className="nav-link text-white" href="/">Entertainment</a>
+                <Link className="nav-link text-white" to="/entertainment">Entertainment</Link>
                 </li>
 
                 <li className="nav-item">
-                <a className="nav-link text-white" href="/">General</a>
+                <Link className="nav-link text-white" to="/general">General</Link>
                 </li>
 
                 <li className="nav-item">
-                <a className="nav-link text-white" href="/">Health</a>
+                <Link className="nav-link text-white" to="/health">Health</Link>
                 </li>
 
                 <li className="nav-item">
-                <a className="nav-link text-white" href="/">Science</a>
+                <Link className="nav-link text-white" to="/science">Science</Link>
                 </li>
 
                 <li className="nav-item">
-                <a className="nav-link text-white" href="/">Sports</a>
+                <Link className="nav-link text-white" to="/sports">Sports</Link>
                 </li>
 
                 <li className="nav-item">
-                <a className="nav-link text-white" href="/">Technology</a>
+                <Link className="nav-link text-white" to="/technology">Technology</Link>
                 </li>
             </ul>
             </div>
             </div>
-        </nav>
-            </div>
+          </nav>
+          </div>
     )
   }
 }
