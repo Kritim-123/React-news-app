7d1ba9b546f83006df4352d1a74fc229f2cdb20f
raged_rhombus <kritimbastola2005@gmail.com>
2024-11-04 15:05:24 -0700

    Added top loader and infinite scroll

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import "./App.css";
 import React, { Component } from "react";
 import NavBar from "./components/NavBar";
 import News from "./components/News";
+import LoadingBar from "react-top-loading-bar";
 
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 export default class App extends Component {
   pageSize = 15;
+
+  state = {
+    progress: 0,
+  };
+
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  };
   render() {
     return (
       <div>
         <BrowserRouter>
           <div>
             <NavBar />
+            <LoadingBar color="#f11946" progress={this.state.progress} />
             <Routes>
               <Route
                 path="/"
                 element={
                   <News
+                    setProgress={this.setProgress}
                     key="general"
                     pageSize={this.pageSize}
                     category="general"
@@ -29,6 +40,7 @@ export default class App extends Component {
                 path="/business"
                 element={
                   <News
+                    setProgress={this.setProgress}
                     key="business"
                     pageSize={this.pageSize}
                     category="business"
@@ -40,6 +52,7 @@ export default class App extends Component {
                 path="/entertainment"
                 element={
                   <News
+                    setProgress={this.setProgress}
                     key="entertainment"
                     pageSize={this.pageSize}
                     category="entertainment"
@@ -51,6 +64,7 @@ export default class App extends Component {
                 path="/general"
                 element={
                   <News
+                    setProgress={this.setProgress}
                     key="general"
                     pageSize={this.pageSize}
                     category="general"
@@ -62,6 +76,7 @@ export default class App extends Component {
                 path="/health"
                 element={
                   <News
+                    setProgress={this.setProgress}
                     key="health"
                     pageSize={this.pageSize}
                     category="health"
@@ -73,6 +88,7 @@ export default class App extends Component {
                 path="/science"
                 element={
                   <News
+                    setProgress={this.setProgress}
                     key="science"
                     pageSize={this.pageSize}
                     category="science"
@@ -84,6 +100,7 @@ export default class App extends Component {
                 path="/sports"
                 element={
                   <News
+                    setProgress={this.setProgress}
                     key="sports"
                     pageSize={this.pageSize}
                     category="sports"
@@ -95,6 +112,7 @@ export default class App extends Component {
                 path="/technology"
                 element={
                   <News
+                    setProgress={this.setProgress}
                     key="technology"
                     pageSize={this.pageSize}
                     category="technology"
